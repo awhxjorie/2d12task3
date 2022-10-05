5e997244b81399db100bbecba42514f31e460e12
awhxjorie <115052145+awhxjorie@users.noreply.github.com>
2022-10-05 09:16:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(4)
+    basic.pause(1000)
+    basic.showNumber(5)
+    basic.pause(1000)
+    basic.showNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
